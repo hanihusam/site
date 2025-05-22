e22cb9a3dd929cdc90ca1244cd82e190e8e7d3cb
Hani Husamuddin <hani.husam@gmail.com>
2025-05-22 10:13:06 +0700

    chore: base asset and metatag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable React to support React JSX components.
-  integrations: [react()],
+	site: 'https://www.bapak2.dev',
+	prefetch: true,
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+	// Enable React to support React JSX components.
+	integrations: [react()],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+})
