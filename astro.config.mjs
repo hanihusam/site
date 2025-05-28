fca305476e34c19de0ef99c2e3c987186df4f920
Hani Husamuddin <hani.husam@gmail.com>
2025-05-28 12:56:08 +0700

    chore: codebase support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.bapak2.dev',
-	prefetch: true,
+  site: "https://www.bapak2.dev",
+  prefetch: true,
 
-	// Enable React to support React JSX components.
-	integrations: [react()],
+  // Enable React to support React JSX components.
+  integrations: [react()],
 
-	vite: {
-		plugins: [tailwindcss()],
-	},
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
--- eslint.config.js
+++ eslint.config.js
@@ -1,60 +1,60 @@
-import { config as defaultConfig } from '@epic-web/config/eslint'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import { config as defaultConfig } from "@epic-web/config/eslint";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
-import tsParser from '@typescript-eslint/parser'
+import tsParser from "@typescript-eslint/parser";
 
-import astro from 'eslint-plugin-astro'
-import astroParser from 'astro-eslint-parser'
+import astro from "eslint-plugin-astro";
+import astroParser from "astro-eslint-parser";
 
 /** @type {import("eslint").Linter.Config} */
 export default [
-	...defaultConfig,
-	{
-		files: ['src/components/react/*.tsx'],
-		plugins: {
-			'simple-import-sort': simpleImportSort,
-		},
-		rules: {
-			'no-console': 'off',
-			'@typescript-eslint/no-floating-promises': 'off', // we don't want to enforce this
+  ...defaultConfig,
+  {
+    files: ["src/components/react/*.tsx"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "no-console": "off",
+      "@typescript-eslint/no-floating-promises": "off", // we don't want to enforce this
 
-			'jsx-a11y/alt-text': 'off', // it's not smart enough
-			'jsx-a11y/media-has-caption': 'off',
-			'no-unused-private-class-members': 'off',
-			'prefer-object-has-own': 'off',
-			'import/newline-after-import': 'warn',
-			'simple-import-sort/exports': 'warn',
+      "jsx-a11y/alt-text": "off", // it's not smart enough
+      "jsx-a11y/media-has-caption": "off",
+      "no-unused-private-class-members": "off",
+      "prefer-object-has-own": "off",
+      "import/newline-after-import": "warn",
+      "simple-import-sort/exports": "warn",
 
-			// this one isn't smart enough for our "@/" imports
-			'import/order': 'off',
+      // this one isn't smart enough for our "@/" imports
+      "import/order": "off",
 
-			'sort-imports': 'off',
-			'simple-import-sort/imports': [
-				'warn',
-				{
-					groups: [
-						['^.+\\.s?css$'],
-						['^\\u0000'],
-						['^react$', '^react-dom$'],
-						['^~', '^@/'],
-						['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-						['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-					],
-				},
-			],
-		},
-	},
-	...astro.configs.recommended,
-	{
-		files: ['**/*.astro'],
-		plugins: {
-			astro,
-		},
-		languageOptions: {
-			parser: astroParser,
-			parserOptions: {
-				parser: tsParser,
-			},
-		},
-	},
-]
+      "sort-imports": "off",
+      "simple-import-sort/imports": [
+        "warn",
+        {
+          groups: [
+            ["^.+\\.s?css$"],
+            ["^\\u0000"],
+            ["^react$", "^react-dom$"],
+            ["^~", "^@/"],
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          ],
+        },
+      ],
+    },
+  },
+  ...astro.configs.recommended,
+  {
+    files: ["**/*.astro"],
+    plugins: {
+      astro,
+    },
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+  },
+];
