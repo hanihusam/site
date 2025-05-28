106622296c367c3f615bddb66210ff2d86655a4a
Hani Husamuddin <hani.husam@gmail.com>
2025-05-28 12:55:52 +0700

    feat: external links

---
+++ src/constant/external-links.ts
@@ -0,0 +1,11 @@
+export const externalLinks = {
+  cv: "https://drive.google.com/file/d/1pauUzeImQPPpzS9D8HI-CcySZO_MjWgk/view?usp=sharing",
+  github: "https://github.com/hanihusam",
+  dribbble: "https://dribbble.com/hanihusam",
+  linkedin: "https://linkedin.com/in/hanihusam",
+  figma: "https://figma.com/@hani.husam",
+  rss: "https://hanihusam.com/blog/rss.xml",
+  email: "https://mailto:hi@hanihusam.com",
+  pitcDeckFigma:
+    "https://www.figma.com/community/file/1016698474340633848/Pitch-Deck-Design-Template",
+};
